4dcf3590df12bd7b013dc6cfb2004d9059ca08e1
psr1919plus21 <psr1919plus21@gmail.com>
2014-01-04 22:43:59 +0200

    Added sort

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
+$(document).ready(function(){
 
+	var SORTER = {};
+SORTER.sort = function(which, dir) {
+  SORTER.dir = (dir == "desc") ? -1 : 1;
+  $(which).each(function() {
+    // Find the list items and sort them
+    var sorted = $(this).find("> li").sort(function(a, b) {
+      return $(a).text().toLowerCase() > $(b).text().toLowerCase() ? SORTER.dir : -SORTER.dir;
+    });
+    $(this).append(sorted);
+  });
+};
+
+
+
+
+
+$('#ascending').click (function() {
+
+  SORTER.sort('.sortable');
+
+});
+
+$('#descending').click(function(){
+	SORTER.sort('.sortable', 'desc');
+});
+
+
+});
